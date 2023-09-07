0852e51a3ffade5c3cc78c1c42177a4e3c405f0a
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-09-07 11:16:22 +0200

    added Laplace and ConformalPrediction

---
+++ test/ConformalPrediction.jl
@@ -0,0 +1,80 @@
+using ConformalPrediction
+using MLJBase
+using MLJLinearModels
+using Plots
+
+isplot(plt) = typeof(plt) <: Plots.Plot
+
+@testset "ConformalPrediction.jl" begin
+
+    @testset "Classification" begin
+
+        # Data:
+        X, y = make_moons(500; noise=0.15)
+        X = MLJBase.table(convert.(Float32, MLJBase.matrix(X)))
+        train, test = partition(eachindex(y), 0.8, shuffle=true)
+
+        # Model:
+        model = MLJLinearModels.LogisticClassifier()
+        conf_model = conformal_model(model; coverage=0.9)
+        mach = machine(conf_model, X, y)
+        fit!(mach, rows=train)
+
+        @test isplot(bar(mach.model, mach.fitresult, X))
+        @test isplot(areaplot(mach.model, mach.fitresult, X, y))
+        @test isplot(
+            areaplot(mach.model, mach.fitresult, X, y; input_var=1)
+        )
+        @test isplot(
+            areaplot(mach.model, mach.fitresult, X, y; input_var=:x1)
+        )
+        @test isplot(contourf(mach.model, mach.fitresult, X, y))
+        @test isplot(
+            contourf(
+                mach.model,
+                mach.fitresult,
+                X,
+                y;
+                zoom=-1,
+                plot_set_size=true
+            ),
+        )
+        @test isplot(
+            contourf(mach.model, mach.fitresult, X, y; target=1)
+        )
+
+    end
+
+    @testset "Regression" begin
+
+        # Data:
+        X, y = make_regression(500)
+        X = MLJBase.table(convert.(Float32, MLJBase.matrix(X)))
+        train, test = partition(eachindex(y), 0.8, shuffle=true)
+
+        # Model:
+        model = MLJLinearModels.LinearRegressor()
+        conf_model = conformal_model(model; coverage=0.9)
+        mach = machine(conf_model, X, y)
+        fit!(mach, rows=train)
+
+        # Plotting:
+        @test isplot(plot(mach.model, mach.fitresult, X, y))
+        @test isplot(
+            plot(
+                mach.model,
+                mach.fitresult,
+                X,
+                y;
+                input_var=1,
+                xlims=(-1, 1),
+                ylims=(-1, 1)
+            ),
+        )
+        @test isplot(
+            plot(mach.model, mach.fitresult, X, y; input_var=:x1)
+        )
+        @test isplot(bar(mach.model, mach.fitresult, X))
+    end
+
+end
