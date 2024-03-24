fba4ec927909c34fc68bf2f90a8c560c7b97b801
pat-alt <altmeyerpat@gmail.com>
2024-03-24 17:37:11 +0100

    cov up for multi-D

--- src/CounterfactualExplations/data.jl
+++ src/CounterfactualExplations/data.jl
@@ -20,7 +20,7 @@ function embed(data::CounterfactualData, X::AbstractArray = nothing; dim_red::Sy
     end
 
     # Transforming:
-    X = typeof(X) <: Vector{<:Matrix} ? stack(X, dims = 2) : X
+    X = typeof(X) <: Vector{<:Matrix} ? hcat(X...) : X
     if !isnothing(tfn) && !isnothing(X)
         X = MultivariateStats.predict(tfn, X)
     else
--- test/CounterfactualExplanations.jl
+++ test/CounterfactualExplanations.jl
@@ -4,22 +4,45 @@ using CounterfactualExplanations.Models
 using Plots
 using TaijaData
 
-# Counteractual data and model:
-counterfactual_data = CounterfactualData(TaijaData.load_linearly_separable()...)
-M = fit_model(counterfactual_data, :Linear)
-target = 2
-factual = 1
-chosen = rand(findall(predict_label(M, counterfactual_data) .== factual))
-x = select_factual(counterfactual_data, chosen)
-
-# Search:
-generator = GenericGenerator()
-ce = generate_counterfactual(x, target, counterfactual_data, M, generator)
-
 @testset "CounterfactualExplanations.jl" begin
-    # Very minimal testing for basic functionality:
-    plot(M, counterfactual_data)
-    plot(ce)
-    TaijaPlotting.animate_path(ce)
+
+    @testset "2D" begin
+
+        # Counteractual data and model:
+        counterfactual_data = CounterfactualData(TaijaData.load_linearly_separable()...)
+        M = fit_model(counterfactual_data, :Linear)
+        target = 2
+        factual = 1
+        chosen = rand(findall(predict_label(M, counterfactual_data) .== factual))
+        x = select_factual(counterfactual_data, chosen)
+
+        # Search:
+        generator = GenericGenerator()
+        ce = generate_counterfactual(x, target, counterfactual_data, M, generator)
+
+        plot(M, counterfactual_data)
+        plot(ce)
+        TaijaPlotting.animate_path(ce)
+    end
+
+    @testset "Multi-dim" begin
+        # Counteractual data and model:
+        counterfactual_data = CounterfactualData(TaijaData.load_blobs(; k = 5)...)
+        M = fit_model(counterfactual_data, :Linear)
+        target = 2
+        factual = 1
+        chosen = rand(findall(predict_label(M, counterfactual_data) .== factual))
+        x = select_factual(counterfactual_data, chosen)
+
+        # Search:
+        generator = GenericGenerator()
+        ce = generate_counterfactual(x, target, counterfactual_data, M, generator)
+
+        # Plots:
+        plot(M, counterfactual_data)
+        plot(ce)
+        TaijaPlotting.animate_path(ce)
+    end
+
     @test true
 end
